cdfbf90372f61969738b6c6f8e477a1ac01ce080
Guest User <Guest@Epicodus-7C.local>
2018-05-08 09:27:46 -0700

    Follow instructions for deleting and editing albums

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -5,17 +5,30 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Injectable()
 export class AlbumService {
   albums: FirebaseListObservable<any[]>;
-  constructor(private database: AngularFireDatabase){ this.albums = database.list('albums');
-}
+  constructor(private database: AngularFireDatabase){
+    this.albums = database.list('albums');
+  }
 
 
   getAlbums() {
     return this.albums;
   }
   addAlbum(newAlbum: Album) {
-  this.albums.push(newAlbum);
-}
+    this.albums.push(newAlbum);
+  }
   getAlbumById(albumId: string){
-  return this.database.object('albums/' + albumId);
- }
+    return this.database.object('albums/' + albumId);
+  }
+
+  updateAlbum(localUpdatedAlbum){
+    var albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
+    albumEntryInFirebase.update({title: localUpdatedAlbum.title,
+    artist: localUpdatedAlbum.artist,
+    description: localUpdatedAlbum.description});
+  }
+
+  deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AdminComponent } from './admin/admin.component';
+import { EditAlbumComponent } from './edit-album/edit-album.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -31,7 +32,8 @@ export const firebaseConfig = {
     AboutComponent,
     MarketplaceComponent,
     AlbumDetailComponent,
-    AdminComponent
+    AdminComponent,
+    EditAlbumComponent
   ],
   imports: [
     BrowserModule,
